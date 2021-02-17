684039284b44a7426025e27971524f8f3e15f22b
Igor <igor.lischewski@web.de>
2021-02-17 12:58:47 +0100

    probelauf

--- js/model.js
+++ js/model.js
@@ -3,8 +3,8 @@
 let data = {
     milk:{lower:0, upper:5, bev:"milch"},
     juice:{lower:6, upper:12, bev:"saft"},
-    cola:{lower:13, upper:17, bev:"cola"},
-    wine:{lower:18, upper:120, bev:"wein"},
+    cola:{lower:13, upper:20, bev:"cola"},
+    wine:{lower:21, upper:110, bev:"wein"},
     default:{bev:"tee"}
 }
 
